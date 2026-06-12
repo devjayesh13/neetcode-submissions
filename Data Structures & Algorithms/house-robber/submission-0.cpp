3bc51b140b0518a5b265b781f74393ff1ded49bc
Jayesh Kumar Gupta <81617201+devjayesh13@users.noreply.github.com>
2026-06-12 23:26:14 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    map<int, int> memo;
+    int rob(vector<int>& nums) {
+        return robbing(0, nums);
+    }
+    int robbing(int k, vector<int>& nums){
+        if(k >= nums.size()) return 0;
+        auto it = memo.find(k);
+        if(it != memo.end()) return it->second;
+        else{
+            int ans = max(robbing(k+1, nums), nums[k] + robbing(k+2, nums));
+            memo[k] = ans;
+            return ans;
+        }
+        return -1;
+    }
+};
