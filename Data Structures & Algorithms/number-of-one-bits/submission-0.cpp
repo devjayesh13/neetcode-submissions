dab23473b50a282cff60c26ea6aad9bbe4e02849
Jayesh Kumar Gupta <81617201+devjayesh13@users.noreply.github.com>
2026-06-13 14:50:20 +0530

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int c = 0;
+        for(int i = 0;i<32;i++){
+            c += (n >> i) & 1;
+        }
+        return c;
+    }
+};
