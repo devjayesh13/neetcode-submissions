f0088d846cd0bd9067af034b88911d35254639ff
Jayesh Kumar Gupta <81617201+devjayesh13@users.noreply.github.com>
2026-07-09 11:15:31 +0530

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> output;
+        output.push_back(intervals[0]);
+
+        for(auto& intv : intervals){
+            int start = intv[0];
+            int end = intv[1];
+            int lastend = output.back()[1];
+
+            if(start <= lastend) output.back()[1] = max(lastend, end);
+            else output.push_back({start, end});
+        }
+        return output;
+    }
+};
